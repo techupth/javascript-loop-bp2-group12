3d33de207d8e0bf3085b016d40bbce03baef9490
Figo <milkylot@gmail.com>
2023-12-14 21:29:31 +0700

    feat: add complete exercise

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,14 @@
 let numbers = [100, 20, 3, 1000];
 let minNumber;
 // Start coding here
+for (let i = 0;i<numbers.length;i++){
+    if(numbers[i]<numbers[i+1]){
+        minNumber = numbers[i]
+    }
+    
+}
+    
+    
+
 
 console.log(minNumber);
--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,8 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
+for (let i in companyName){
+    console.log(`Number ${(+i+1)}\ncharacter is ${companyName[i]}`)
 
+
+}
 // Start coding here
